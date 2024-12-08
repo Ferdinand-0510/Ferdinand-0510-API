440b5c8c76c6f8cfdb67d276ebaf5231f3811112
Ferdinand--0510 <xoaup3857@gmail.com>
2024-12-08 11:02:25 +0800

    更新數據庫連接配置

--- database.py
+++ database.py
@@ -20,46 +20,55 @@ def get_odbc_drivers():
         return []
 
 def create_connection():
-    """创建与 Azure SQL Database 的连接"""
+    """創建與 Azure SQL Database 的連接"""
     server = "carlweb-server.database.windows.net"
     database = "CarlWeb"
     username = "carluser"
     password = os.getenv('DB_PASSWORD')
 
-    # 先检查可用驱动
-    available_drivers = get_odbc_drivers()
-    logger.debug(f"可用驱动: {available_drivers}")
+    # 獲取可用的驅動程序
+    drivers = pyodbc.drivers()
+    logger.info(f"可用的 ODBC 驅動: {drivers}")
 
-    try:
-        # 尝试使用多个驱动名称
-        driver_attempts = [
-            "ODBC Driver 17 for SQL Server",
-            "SQL Server",
-            "FreeTDS"
-        ]
+    # 嘗試不同的驅動程序
+    driver_attempts = [
+        "ODBC Driver 18 for SQL Server",  # 首選 18 版本
+        "ODBC Driver 17 for SQL Server",
+        "msodbcsql18",
+        "msodbcsql17"
+    ]
 
-        for driver in driver_attempts:
-            try:
-                conn_str = (
-                    f'DRIVER={{{driver}}};'
-                    f'SERVER={server};'
-                    f'DATABASE={database};'
-                    f'UID={username};'
-                    f'PWD={password};'
-                    'Encrypt=yes;'
-                )
+    last_error = None
+    for driver in driver_attempts:
+        try:
+            conn_str = (
+                f'DRIVER={{{driver}}};'
+                f'SERVER={server};'
+                f'DATABASE={database};'
+                f'UID={username};'
+                f'PWD={password};'
+                'Encrypt=yes;'
+                'TrustServerCertificate=yes;'  # 添加此行以處理 SSL 問題
+            )
 
-                logger.debug(f"尝试使用驱动 {driver}")
-                conn = pyodbc.connect(conn_str)
-                logger.info(f"成功使用 {driver} 连接到数据库")
-                return conn
+            logger.info(f"嘗試使用驅動: {driver}")
+            conn = pyodbc.connect(conn_str)
+            logger.info(f"成功使用 {driver} 連接到數據庫")
+            return conn
 
-            except Exception as driver_error:
-                logger.warning(f"使用 {driver} 连接失败: {driver_error}")
+        except Exception as e:
+            last_error = e
+            logger.warning(f"使用 {driver} 連接失敗: {str(e)}")
+            continue
 
-        # 如果所有尝试都失败
-        raise Exception("无法建立数据库连接")
-
-    except Exception as e:
-        logger.error(f"数据库连接错误: {e}")
-        raise
+    # 如果所有嘗試都失敗
+    error_msg = f"""
+    數據庫連接失敗:
+    最後錯誤: {str(last_error)}
+    可用驅動: {drivers}
+    服務器: {server}
+    數據庫: {database}
+    用戶名: {username}
+    """
+    logger.error(error_msg)
+    raise Exception(error_msg)
